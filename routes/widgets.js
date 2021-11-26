d4657bc5c21dbd9eb56aca4f29bf519927d72251
Dominic Tremblay <dominictremblay24@gmail.com>
2021-11-26 13:22:35 -0500

    adding users routing users

--- routes/widgets.js
+++ routes/widgets.js
@@ -6,24 +6,39 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
+  router.get('/', (req, res) => {
     let query = `SELECT * FROM widgets`;
     console.log(query);
     db.query(query)
-      .then(data => {
+      .then((data) => {
         const widgets = data.rows;
-        const templateVars = {widgets};
+        const templateVars = { widgets };
         console.log(templateVars);
-        res.json({widgets})
+        res.json({ widgets });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
+  });
+
+  // /api/widgets/users
+  router.get('/users', (req, res) => {
+    const queryText = {
+      text: `SELECT users.id as user_id, users.name as username, widgets.id as widget_id, widgets.name as widget_name
+      FROM users
+      INNER JOIN widgets
+      ON users.id = widgets.user_id
+      `,
+    };
+
+    db.query(queryText)
+      .then((data) => {
+        res.json({ userWidgets: data.rows });
+      })
+      .catch((err) => console.log(err.message));
   });
   return router;
 };
